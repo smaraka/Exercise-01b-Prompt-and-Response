22e4adec5ad952599d72b328df3ed53d3c236f6d
smaraka <smaraka@iu.edu>
2021-08-28 19:08:57 -0400

    Update main.py
    Added print statements to utilize the variables in order to simulate what we are going to do in our game at the base level with the player to game interaction. Also added my response to the question at the bottom.

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
 passage_name = "West of House"
 passage_text = "This is an open field west of a white house, with a boarded front door."
+
+print("You are at the", passage_name)
+print(passage_text)
+print("What would you like to do? ")
+response = input()
+if response == "go north":
+    print("Good Job!")
+
+# Try typing "Go north" or "Go North". Why do you think it behaved like that?
+# because the string has to be exactly matching in terms of punctuation and case sensitivity
